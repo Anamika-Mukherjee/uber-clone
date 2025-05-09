0aef25379d0348872ca2e5b28d3421e774ed0c75
Anamika Mukherjee <anamikamukherjee.raipur@gmail.com>
2025-05-09 20:54:47 +0530

    Firebase initialization

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,14 +1,20 @@
 import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
 
 const serviceAccount = require("./firebase-service-account-key.json");
 
-if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    }),
+  });
 }
 
 export const db = getFirestore();
 export default admin;
-  
+
+
